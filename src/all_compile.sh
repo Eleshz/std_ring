e947593770e0c91a51f21fc35a14db6f427afaa3
Edgar Hammond <edgarhammond@gmail.com>
2024-03-20 22:25:37 -0400

    Cleaner

--- src/all_compile.sh
+++
@@ -1,4 +0,0 @@
-g++ -std=c++11 -g -O0 main.cpp -o DEBUG_RING-11
-g++ -std=c++2b -g -O0 main.cpp -o DEBUG_RING-23
-g++ -std=c++11 -O3 main.cpp -o OMTIM_Ring-11
-g++ -std=c++2b -O3 main.cpp -o OMTIM_Ring-23
